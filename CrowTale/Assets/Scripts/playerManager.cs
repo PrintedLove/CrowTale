bc2d18ff608ed9ae69570cfe8c6cb74206f52841
Printed Love <dnjswn0328@gmail.com>
2021-08-13 16:09:35 +0900

    플레이어 점프 추가

--- CrowTale/Assets/Scripts/playerManager.cs
+++ CrowTale/Assets/Scripts/playerManager.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class playerManager : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpSpeed;
     public float horizontalFriction;
     public float runStopSpeed;
+
+    private bool isGrounded;
+    private bool isFly;
 
     Rigidbody2D rigidBody;
     SpriteRenderer spriteRenderer;
@@ -17,11 +21,38 @@ public class playerManager : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+
+        isFly = false;
     }
 
     void Update()
     {
-        if (Input.GetButtonUp("Horizontal"))        // 좌우 정지시 마찰
+        if (Input.GetButtonDown("Jump"))        //점프
+        {
+            if (isGrounded)
+            {
+                rigidBody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+                animator.SetBool("isJump", true);
+            } else
+            {
+                if (isFly)
+                {
+                    rigidBody.AddForce(Vector2.up * jumpSpeed * 0.75f, ForceMode2D.Impulse);
+                    animator.SetBool("isFly", true);
+                    rigidBody.drag = 10;
+                    rigidBody.gravityScale = 1;
+                }
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            animator.SetBool("isJump", false);
+            animator.SetBool("isFly", false);
+            isFly = true;
+        }
+
+        if (Input.GetButtonUp("Horizontal"))        //좌우 정지시 마찰
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.normalized.x * horizontalFriction, rigidBody.velocity.y);
         }
@@ -36,7 +67,7 @@ public class playerManager : MonoBehaviour
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        rigidBody.AddForce(Vector2.right * horizontalInput * 2, ForceMode2D.Impulse);   // 플레이어 좌우 이동
+        rigidBody.AddForce(Vector2.right * horizontalInput * 2, ForceMode2D.Impulse);   //플레이어 좌우 이동
 
         if (horizontalInput < 0)            //플레이어 방향
             spriteRenderer.flipX = true;
@@ -44,11 +75,26 @@ public class playerManager : MonoBehaviour
             spriteRenderer.flipX = false;
 
         if (rigidBody.velocity.x > maxSpeed)    //좌우 최대 속도
-        {
             rigidBody.velocity = new Vector2(maxSpeed, rigidBody.velocity.y);
-        } else if (rigidBody.velocity.x < maxSpeed * (-1))
-        {
+        else if (rigidBody.velocity.x < maxSpeed * (-1))
             rigidBody.velocity = new Vector2(maxSpeed * (-1), rigidBody.velocity.y);
+
+        //바닥에 닿았는지 계산
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rigidBody.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if (rayHit.collider != null && rayHit.distance < 0.75f)
+        {
+            isGrounded = true;
+            animator.SetBool("isGrounded", true);
+            isFly = false;
+            rigidBody.drag = 3;
+            rigidBody.gravityScale = 2;
+        }
+        else
+        {
+            isGrounded = false;
+            animator.SetBool("isGrounded", false);
         }
     }
 }
