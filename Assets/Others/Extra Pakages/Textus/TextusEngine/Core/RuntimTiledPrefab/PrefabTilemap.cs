3f373c13ea735d3fe131d5173a76c912d97e8a27
Printed Love <60221643+PrintedLove@users.noreply.github.com>
2022-04-19 01:12:14 +0900

    프리팹 타일 패키지 추가. 타일 구조 변경.

---
+++ Assets/Others/Extra Pakages/Textus/TextusEngine/Core/RuntimTiledPrefab/PrefabTilemap.cs
@@ -0,0 +1,28 @@
+﻿// Creator: TextusGames
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class PrefabTilemap : MonoBehaviour
+{
+    void Start()
+    {
+        var map = GetComponent<Tilemap>();
+
+        foreach (var pos in map.cellBounds.allPositionsWithin)
+        {   
+            Vector3Int localPlace = new Vector3Int(pos.x, pos.y, pos.z);
+            //Vector3 place = map.CellToWorld(localPlace);
+            if (map.HasTile(localPlace))
+            {   
+                var tilePrefab = map.GetTile<PrefabTile>(localPlace);
+                //print(tilePrefab);
+                if (!tilePrefab) continue;
+                tilePrefab.InstantiatePrefab(map,localPlace);
+            }
+        }
+
+    }
+   
+}
