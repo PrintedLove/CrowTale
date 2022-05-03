703e2a7b7f186601acb44f06ee6e817e09ec50b7
Printed Love <60221643+PrintedLove@users.noreply.github.com>
2022-05-03 21:01:47 +0900

    Decoration Tile Generator에 주석및 약간의 수정

--- Assets/Scripts/DecoratiionTileGenerator.cs
+++ Assets/Scripts/DecoratiionTileGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+// 타일 그룹 종류
 enum Tiletype:int
 {
     Stone_1, Stone_2, Stone_3, Stone_4, Bush_1, Bush_2, Bush_3, Bush_4
@@ -11,6 +12,7 @@ enum Tiletype:int
 
 public class DecoratiionTileGenerator : MonoBehaviour
 {
+    //타일 그룹 종류에 따른 타일 리스트 저장용 구조체
     struct DecoratiionTile
     {
         public Tiletype name;
@@ -35,6 +37,7 @@ public class DecoratiionTileGenerator : MonoBehaviour
     List<DecoratiionTile> Tileset = new List<DecoratiionTile>();
 
     private void Awake() {
+        //타일 그룹 리스트에에 타일리스트 저장
         Tileset.Add(new DecoratiionTile(Tiletype.Stone_1, tiles[0]));
         Tileset.Add(new DecoratiionTile(Tiletype.Stone_2, tiles[1]));
         Tileset.Add(new DecoratiionTile(Tiletype.Stone_3, tiles[2]));
@@ -58,37 +61,40 @@ public class DecoratiionTileGenerator : MonoBehaviour
     }
 
     private void LateUpdate() {
+        //다른 타일들의 위치정보를 받아오기 위해서 후순위 업데이트문에서 타일 생성
         GenerateTile();
         Destroy(this);
     }
 
     void GenerateTile()
     {
+        //Ground Rule Tile(Assets - Others - Tilemaps)에 의해 상단이 빈공간인 플랫폼 타일위에 생성된 오브젝트 리스트를 받아옴.
         GameObject[] objList = GameObject.FindGameObjectsWithTag("Decoration Tile Generator");
 
         for (int i = 0; i < objList.Length; i++)
         {
-            Vector3 objPosRaw = objList[i].GetComponent<Transform>().position;
-            Vector3Int objPos = new Vector3Int((int)objPosRaw.x, (int)objPosRaw.y, 0);
-            Vector3Int setPos = new Vector3Int(objPos.x, objPos.y + 1, objPos.z);
+            Vector3 objPosRaw = objList[i].GetComponent<Transform>().position;      //실제 오브젝트 위치
+            Vector3Int objPos = new Vector3Int((int)objPosRaw.x, (int)objPosRaw.y, 0);      //float -> Int 처리한 오브젝트 위치
+            Vector3Int setPos = new Vector3Int(objPos.x, objPos.y + 1, objPos.z);           //타일 생성 시작 위치
 
             if (calProbability(35))
             {
                  int tiletype;
 
+                //하단에 플랫폼 타일이 위치하고, 생성할 위치에 플랫폼 타일이 없을 경우.
                 if (tilemap_Ground.GetComponent<Tilemap>().GetTile(objPos) != null
-                 && tilemap_Ground.GetComponent<Tilemap>().GetTile(new Vector3Int(objPos.x, objPos.y + 1, objPos.z)) == null
-                 && tilemap_Objects.GetComponent<Tilemap>().GetTile(new Vector3Int(objPos.x, objPos.y + 1, -1)) == null) {
-                    if(calProbability(19))
+                 && tilemap_Ground.GetComponent<Tilemap>().GetTile(new Vector3Int(objPos.x, objPos.y + 1, objPos.z)) == null) {
+                    if(calProbability(19))      // 19%확률로 나무 생성
                     {
+                        //생성 위치에 다른 데코레이션 타일이 없을 경우
                         if(tilemap_Ground.GetComponent<Tilemap>().GetTile(new Vector3Int(objPos.x + 1, objPos.y, objPos.z)) != null
-                         && tilemap_Objects.GetComponent<Tilemap>().GetTile(new Vector3Int(objPos.x + 1, objPos.y + 1, objPos.z)) == null
                          && tilemap_Stones.GetComponent<Tilemap>().GetTile(new Vector3Int(objPos.x + 1, objPos.y + 1, objPos.z)) == null
                          && tilemap_Bushs.GetComponent<Tilemap>().GetTile(new Vector3Int(objPos.x + 1, objPos.y + 1, objPos.z)) == null)
                         {
                             tiletype = Random.Range((int)Tiletype.Tree_1, (int)Tiletype.Tree_6 + 1);
                             bool isGenOk = true;
 
+                            //생성 위치에 같은 타일맵의 타일 그룹이 겹치지 않는지 검사
                             for (int n = 0; n < Tileset[tiletype].tileList.Length; n++)
                             {
                                 if(tilemap_Trees.GetComponent<Tilemap>().GetTile(new Vector3Int(setPos.x + (n % 2), setPos.y + n / 2, objPos.z)) != null)
@@ -107,7 +113,7 @@ public class DecoratiionTileGenerator : MonoBehaviour
                     }
                     else 
                     {
-                        if(calProbability(38))
+                        if(calProbability(38))  //나무가 아닐시 38% 확률로 암석 생성
                         {
                             tiletype = Random.Range((int)Tiletype.Stone_1, (int)Tiletype.Stone_4 + 1);
                             tilemap_Stones.GetComponent<Tilemap>().SetTile(setPos, Tileset[tiletype].tileList[0]);
@@ -121,17 +127,17 @@ public class DecoratiionTileGenerator : MonoBehaviour
                 }
             }
 
-            Destroy(objList[i].gameObject);
+            Destroy(objList[i].gameObject);     //위치 감지용 오브젝트 제거
             objList[i] = null;
         }
 
         tilemap_Stones.GetComponent<TilemapCollider2D>().enabled = true;
-        tilemap_Stones.GetComponent<TilemapShadowCaster2DCustom>().MakeShadow();
+        tilemap_Stones.GetComponent<TilemapShadowCaster2DCustom>().MakeShadow();    //쉐도우캐스터 동적 생성
         tilemap_Bushs.GetComponent<TilemapCollider2D>().enabled = true;
         tilemap_Bushs.GetComponent<TilemapShadowCaster2DCustom>().MakeShadow();
     }
 
-    bool calProbability(byte chance)
+    bool calProbability(byte chance)    //퍼센트 확률 계산용 함수
     {
         if (chance >= Random.Range(1, 101))
             return true;
