68a5e4f111ada934ef3ed34a411aa2637253d3aa
PrintedLove <dnjswn0328@gmail.com>
2022-11-15 23:08:53 +0900

    some change in gameManager, font, story

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -150,8 +150,9 @@ public class GameManager : MonoBehaviour
             titleMenu.GetComponent<TitleMenuController>().SetTitleEnd();
         } else
         {
-            mainCamera.transform.position = new Vector3(-70f, 24f, -10f);
+            mainCamera.transform.position = new Vector3(-110f, 24f, -10f);
             player.transform.position = new Vector3(-70f, 4f, -0f);
+            respawnPosition = new Vector3(-70f, 4f, -0f);
             ingameUI.SetActive(false);
         }
 
